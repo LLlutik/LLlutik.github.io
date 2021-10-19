f867cebd2c283e151bc9fa9a9d1adea859337142
LLlutik <87861373+LLlutik@users.noreply.github.com>
2021-10-19 21:28:01 +0300

    last step

--- cats/js/main.js
+++ cats/js/main.js
@@ -5,6 +5,28 @@ $(function(){
     });
     $('.menu-close').on('click',function(){
         $('.header__menu').hide();
+    });
+    $('.btn-like').on('click',function(){
+        $(this).addClass('red');
+        $(this).removeClass('btn-like2')
+        alert('Вы добавили данный товар в избранное!');
+    });
+    $.validator.addMethod('regex', function(value, elenent,regexp){
+        let regExp = new RegExp(regexp);
+        return regExp.test(value);
+    }, 'Please check your input');
+    
+    $('form').validate({
+        rules:{
+            email:{
+                required: true,
+                regex : "[A-Za-z]{1,32}@"
+            }
+        },  
+        messages: {
+            email: 'Введите почту правильно'
+        }
     })
+
 });
 
